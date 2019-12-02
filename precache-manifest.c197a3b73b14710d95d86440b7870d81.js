8b9a7168153d1988455d37f88f402d22beed37c7
SerejKa-lab <seruogacom@mail.ru>
2019-12-02 14:34:51 +0300

    Updates

---
+++ precache-manifest.c197a3b73b14710d95d86440b7870d81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd695a7a330c17929aa754080fae5c1",
+    "url": "/TasksManager/index.html"
+  },
+  {
+    "revision": "8053b781ecc48f5f396a",
+    "url": "/TasksManager/static/css/main.ed16cc4d.chunk.css"
+  },
+  {
+    "revision": "0593ecd97a3154b920e1",
+    "url": "/TasksManager/static/js/2.39021a73.chunk.js"
+  },
+  {
+    "revision": "8053b781ecc48f5f396a",
+    "url": "/TasksManager/static/js/main.6f16fffb.chunk.js"
+  },
+  {
+    "revision": "72b835dc400171ebff7a",
+    "url": "/TasksManager/static/js/runtime~main.deb207a2.js"
+  }
+]);

402a5c301d7411530bbca71ac926a11af972e6ff
SerejKa-lab <seruogacom@mail.ru>
2019-12-03 10:49:43 +0300

    navLink refactored

--- src/ui/Navigation.js
+++ src/ui/Navigation.js
@@ -3,7 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = ( { linkData } ) => {
  
-const listsLinks = linkData.map( (link) => <NavLink to={`list ${link.id}`}  >{link.title}</NavLink> )
+const listsLinks = linkData.map( (link) => {
+    return (
+        <NavLink to={`list ${link.id}`}  >{link.title}</NavLink> 
+        ) }
+    )
     return (
         <nav className='app_navigation'>
             {listsLinks}

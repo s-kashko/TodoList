9e61e3299fa57f0eda3d0af9d22726ff057b1cad
SerejKa-lab <seruogacom@mail.ru>
2019-12-03 10:47:59 +0300

    project folder renaming

---
+++ build/precache-manifest.150f314ace68ea6950dd7e7e16346af0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b18b15efc23015929327d11a82df075",
+    "url": "/TasksManager/index.html"
+  },
+  {
+    "revision": "7d6f2cb4e2576968d76b",
+    "url": "/TasksManager/static/css/main.f5b047c2.chunk.css"
+  },
+  {
+    "revision": "0593ecd97a3154b920e1",
+    "url": "/TasksManager/static/js/2.39021a73.chunk.js"
+  },
+  {
+    "revision": "7d6f2cb4e2576968d76b",
+    "url": "/TasksManager/static/js/main.6f16fffb.chunk.js"
+  },
+  {
+    "revision": "72b835dc400171ebff7a",
+    "url": "/TasksManager/static/js/runtime~main.deb207a2.js"
+  }
+]);
